947c360e8ad049283c03eca00722fd86a45250cd
EnGulSax <adamcarlleo@gmail.com>
2020-12-07 10:47:47 +0100

    Change project view, started working on modal pop up

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+7/*document.addEventListener("DOMContentLoaded", () => {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var card = document.getElementById("slick-slide00");
+  console.log(card)
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  card.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+ span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+});*/
--- js/project-generator.js
+++ js/project-generator.js
@@ -3,46 +3,30 @@ document.addEventListener("DOMContentLoaded", () => {
     var json = JSON.parse(text);
 
     var sliderNav = document.getElementById("slider-nav");
-    var projectView = document.getElementById("slider-for");
+    //var projectView = document.getElementById("slider-for");
 
     for (var project of json["projects"]) {
       navSetup(project, sliderNav, document);
-      projectSetup(project, projectView, document);
+      //projectSetup(project, projectView, document);
     }
-
     setUpSlick();
+    setUpModal(json);
   });
 });
 
 function setUpSlick() {
-  $(".slider-for").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: ".slider-nav",
-  });
-
   $(".slider-nav").slick({
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
-    asNavFor: ".slider-for",
     dots: true,
     arrows: false,
     focusOnSelect: true,
     responsive: [
       {
-        breakpoint: 1020,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
         breakpoint: 800,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
         },
       },
     ],
@@ -64,47 +48,79 @@ function loadJSON2(callback) {
 
 function navSetup(project, sliderNav, document) {
   var img = document.createElement("img");
+  img.dataset.target = "#myModal";
   img.src = project["img"];
   img.alt = project["name"];
+  img.classList.add("id");
   img.classList.add("project");
   sliderNav.appendChild(img);
 }
 
 function projectSetup(project, projectView, document) {
-  var div = document.createElement("div");
-
-  div.classList.add("card")
+  div.classList.add("card");
 
   var h2 = document.createElement("h2");
   h2.innerText = project["name"];
   h2.classList.add("smallmedium-h-size", "font-raleway");
   div.append(h2);
 
-  var ul = document.createElement("ul")
-  ul.classList.add("tags")
-  for(var tag of project["tags"]){
-    var li = document.createElement("li")
-    li.innerText = tag
-    li.classList.add("tags")
-    ul.append(li)
+  var ul = document.createElement("ul");
+  ul.classList.add("tags");
+  for (var tag of project["tags"]) {
+    var li = document.createElement("li");
+    li.innerText = tag;
+    li.classList.add("tags");
+    ul.append(li);
   }
-  div.append(ul)
+  div.append(ul);
 
   var p = document.createElement("p");
   p.classList.add("font-poiret", "text-p-size");
   p.innerText = project["desc"];
   div.append(p);
 
-
   var div2 = document.createElement("div");
-  div2.classList.add("container")
-  var iframe = document.createElement("iframe")
-  iframe.src = project["video"]
-  iframe.classList.add("responsive-iframe")
-  div2.append(iframe)
-  div.append(div2)
-
-
+  div2.classList.add("container");
+  var iframe = document.createElement("iframe");
+  iframe.src = project["video"];
+  iframe.classList.add("responsive-iframe");
+  div2.append(iframe);
+  div.append(div2);
 
   projectView.appendChild(div);
+}
+
+/* MODAL */
+function setUpModal(json) {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  /*Only works for as long as projects num is under 10 (which it will be)*/
+  var i = 0;
+  json["projects"].forEach(() => {    
+    // Get the button that opens the modal
+    var card = document.getElementById("slick-slide0" + i);
+    console.log(card)
+    // When the user clicks on the button, open the modal
+    card.onclick = function () {
+      modal.style.display = "block";
+    };
+    i++;
+  })
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 }
